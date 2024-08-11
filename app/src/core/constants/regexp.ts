7a11863b466c494ed234e4085bce848b74a87f61
estanip <estani.pettigrew@gmail.com>
2024-08-11 12:51:11 -0300

    feat: set environments

--- app/src/core/constants/regexp.ts
+++
@@ -1,3 +0,0 @@
-export const RegExp = {
-  EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
-};
--- app/src/shared/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export enum MatFormFieldAppearances {
-  FILL = 'fill',
-  OUTLINE = 'outline',
-}
