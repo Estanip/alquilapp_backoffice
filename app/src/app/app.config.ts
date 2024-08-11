34beecb9cfe27dcfa51edee19ab12e9355813079
estanip <estani.pettigrew@gmail.com>
2024-08-11 13:07:25 -0300

    feat: set interceptors & JWT service

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,12 +1,18 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { authInterceptor, errorHandlerInterceptor } from '@app/app/core/interceptors';
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authInterceptor, errorHandlerInterceptor])),
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
   ],
 };
